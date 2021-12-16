fecef7e0efc6868dc6147dfc4f4f6f1b180a47fc
trippy22 <trippy22@desktop.local>
2021-12-16 12:11:27 -0500

    new client

--- run.bat
+++ run.bat
@@ -1,3 +1,3 @@
 @echo off
-java -Drunelite.pluginhub.version=1.8.7-SNAPSHOT -jar client-1.8.7-SNAPSHOT-shaded.jar
+java -Drunelite.pluginhub.version=1.8.7 -jar client-1.8.8-SNAPSHOT-shaded.jar
 pause
